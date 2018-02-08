7f2ae71d49ad693e003c089289142be8ea36f8ae
KyleWestendorf <kylewestendorf@Gmail.com>
2018-02-08 13:28:16 -0500

    Adding Gdax interface. Works.

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+        public class Body
+        {
+            public string price { get; set; }
+            public string size { get; set; }
+            public string side { get; set; }
+            public string product_id { get; set; }
+        }
+ }
+
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,12 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+
 
 namespace WpfApp1
 {
@@ -23,6 +28,52 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static byte[] HashHMAC(byte[] key, byte[] message)
+        {
+            var hash = new HMACSHA256(key);
+            return hash.ComputeHash(message);
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        //public void Authenticate()
+        //{
+        //    string time = DateTime.UtcNow.ToString("o");
+	       // string secret = "";
+        //    string method = "POST";
+        //    string requestPath = "/orders";
+        //    Body body = new Body();
+
+        //        body.price = "1.0";
+        //        body.size = "1.0";
+        //        body.side = "BUY";
+        //        body.product_id = "BTC-USD";
+
+        //    string newBody = JsonConvert.SerializeObject(body);
+
+        //    string message = time + method + requestPath + newBody;
+        //    string encodedMessage = Base64Encode(message);
+        //    Byte[] encodedMessageAsByte = System.Text.Encoding.UTF8.GetBytes(encodedMessage);
+        //    Byte[] data = Convert.FromBase64String(secret);
+        //    string decodedString = Encoding.UTF8.GetString(data);
+            
+        //    HMACSHA256 secretHmacsha256 = new HMACSHA256();
+        //    label3.Content = System.Text.Encoding.UTF8.GetString( secretHmacsha256.ComputeHash(encodedMessageAsByte));
+
+
+        //     You're trying to figure out string vs. byte. You made it to step four but can't figure out how to encode hmacsha256
+        //}
+
         public MainWindow()
         {
             InitializeComponent();
@@ -38,11 +89,10 @@ namespace WpfApp1
             // Display the contents of the page to the console.
             Stream streamResponse = myHttpWebResponse.GetResponseStream();
             StreamReader streamRead = new StreamReader(streamResponse);
-            Char[] readBuff = new Char[256];
-            //int count = streamRead.Read(readBuff, 0, 256);
             string myResult = streamRead.ReadToEnd();
             var coinData = JsonConvert.DeserializeObject<Coin>(myResult);
             string allBids = "";
+            
 
             for (int i = 0; i < 50; i++)
             {
@@ -59,13 +109,29 @@ namespace WpfApp1
                 asksList.Items.Add(asks);
             }
 
-            
-            
+            //Authenticate();
+
 
             streamRead.Close();
             streamResponse.Close();
             myHttpWebResponse.Close();
             Console.ReadLine();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Create a new 'HttpWebRequest' object to the mentioned URL.
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.gdax.com/products/BTC-USD/book?level=2");
+            myHttpWebRequest.UserAgent = ".NET Framework Test Client";
+            myHttpWebRequest.Method = "POST";
+            myHttpWebRequest.Timeout = 12000;
+            // Assign the response object of 'HttpWebRequest' to a 'HttpWebResponse' variable.
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            // Display the contents of the page to the console.
+            Stream streamResponse = myHttpWebResponse.GetResponseStream();
+            StreamReader streamRead = new StreamReader(streamResponse);
+            string myResult = streamRead.ReadToEnd();
+            var coinData = JsonConvert.DeserializeObject<Coin>(myResult);
         }
 
        
